f4ec0432dbec248cc6fb1d8384c33086648eb61e
demondcn <guerrero70407@gmail.com>
2024-08-07 20:12:46 -0500

    cambios de 1/08/2024

--- proyecto de grado LT/Software/Lactato_Udec/app/src/main/java/com/example/lactato_udec/prueba_de_carrera_de_pista_activity.java
+++ proyecto de grado LT/Software/Lactato_Udec/app/src/main/java/com/example/lactato_udec/prueba_de_carrera_de_pista_activity.java
@@ -70,6 +70,10 @@ public class prueba_de_carrera_de_pista_activity extends AppCompatActivity {
 	private boolean Anaerobica = false;
 	private boolean Aerobico = true;
 	private ScatterPlot scatterPlot;
+
+	//agregage de textViewHints
+	private TextView hintTextView;
+	private TextView hintTextViewFCLPM;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 
@@ -114,8 +118,10 @@ public class prueba_de_carrera_de_pista_activity extends AppCompatActivity {
 		guardar = (TextView) findViewById(R.id.guardar);
 		__ya_definiste_tus_datos_ = (TextView) findViewById(R.id.__ya_definiste_tus_datos_);
 		nota__el_calentamiento_no_puede_ser_de_mas_de_15_min__ni_incluyendo_piques = (TextView) findViewById(R.id.nota__el_calentamiento_no_puede_ser_de_mas_de_15_min__ni_incluyendo_piques);
-	
-		
+		//textViews
+		hintTextView = (TextView) findViewById(R.id.hintTextView);
+		hintTextViewFCLPM = (TextView) findViewById(R.id.hintTextViewFCLPM);
+
 		//custom code goes here
 		findViewById(R.id.tartan).setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -185,6 +191,8 @@ public class prueba_de_carrera_de_pista_activity extends AppCompatActivity {
 			// Guardar datos
 			m += 1;
 			Distancia.setVisibility(View.GONE);
+			hintTextView.setVisibility(View.GONE);
+
 			if (EtapasIniciales[n][3] < 4){
 				etapa_1_aerobica.setText("Etapa " + m + " Aerobica");
 				Aerobico = true;
@@ -192,7 +200,9 @@ public class prueba_de_carrera_de_pista_activity extends AppCompatActivity {
 			else {
 				etapa_1_aerobica.setText("Etapa Anaerobica");
 				FCLPM.setHint("TRAMO PARA CONTROL DE RITMO, M.");
+				hintTextViewFCLPM.setVisibility(View.GONE);
 				Distancia.setVisibility(View.VISIBLE);
+				hintTextView.setVisibility(View.VISIBLE);
 				Anaerobica = true;
 				Aerobico = false;
 			}
